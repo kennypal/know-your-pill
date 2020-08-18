aecc766d30e2f9f1c17da6986a52da9efc31e324
Kenneth Palces <kennypal@Kenneths-MacBook-Pro.local>
2020-08-18 10:22:20 -0400

    updated

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const getImprint = async () => {
-  const url = 'https://rximage.nlm.nih.gov/api/rximage/1/rxbase?imprint=10,g,1540'
+// const url = `https://rximage.nlm.nih.gov/api/rximage/1/rxbase?parse=${imprint}`
+
+const getImprint = async (imprint) => {
+  const url = `https://rximage.nlm.nih.gov/api/rximage/1/rxbase?parse=${imprint}`
+  
+
   try {
     const response = await axios.get(url)
-    console.log(response.data.nlmRxImages[0].imageUrl)
+    console.log(response.data.nlmRxImages)
 
   } catch (error) {
     console.log(`Error: ${error}`)
   }
 }
 
-getImprint ()
+const submit = document.querySelector('#imprint-form')
+submit.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const input = document.querySelector('#imprint-1').value
+  getImprint(input)
+})
