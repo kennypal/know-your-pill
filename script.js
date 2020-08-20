8a41feb711897f47f5f183030095bf894d906c6b
Kenneth Palces <kennypal@Kenneths-MacBook-Pro.local>
2020-08-20 10:57:34 -0400

    updated with responsive design

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const getData = async (imprint, imprint2) => {
-  // console.log(imprint, imprint2)
+  
   let url;
   if (imprint !== '' && imprint2 !== '') {
     url = `https://rximage.nlm.nih.gov/api/rximage/1/rxbase?parse=${imprint},${imprint2}`
@@ -7,16 +7,14 @@ const getData = async (imprint, imprint2) => {
     url = `https://rximage.nlm.nih.gov/api/rximage/1/rxbase?parse=${imprint}`
   }
 
-  // console.log(url)
   try {
     const response = await axios.get(url)
     const res = (response.data.nlmRxImages)
-    // const drugList = document.querySelector('.drug-list')
 
     removeDrug()
 
     res.forEach((drug) => {
-      // console.log(drug.name)
+      
       const drugList = document.querySelector('.drug-list')
 
       const drugDiv = document.createElement('div')
@@ -52,11 +50,8 @@ const getData = async (imprint, imprint2) => {
 function getValues(e) {
   e.preventDefault()
   const input = document.querySelector('#imprint-1').value
-  // console.log(input)
   const input2 = document.querySelector('#imprint-2').value
-  // console.log(input2)
-  // const input3 = document.querySelector('#color').value
-  // const input4 = document.querySelector('#shape').value
+
   document.querySelector('#imprint-1').value = ''
   document.querySelector('#imprint-2').value = ''
   getData(input, input2)
@@ -65,8 +60,6 @@ const submit = document.querySelector('#imprint-form')
 submit.addEventListener('submit', getValues)
 
 function removeDrug() {
-  // write code here
-  // Make sure to call this function???
    const appendElement = document.querySelector('.drug-list')
    while (appendElement.lastChild) {
      appendElement.removeChild(appendElement.lastChild)
